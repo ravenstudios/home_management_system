db4d5f7ea5dca9d56fdb097925a91786892d319b
ravenstudios <ravenstudiosrob@yahoo.com>
2022-10-30 13:10:56 -0500

    updated DOCKERFILE

--- home_management_system/home_management_system/settings.py
+++ home_management_system/home_management_system/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'home_management_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR  'db.sqlite3',
+        'NAME': BASE_DIR / 'hms_db/db.sqlite3',
     }
 }
 
