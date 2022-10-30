12c468d9418c17a0408265e9fe1e75913f948bd1
ravenstudios <ravenstudiosrob@yahoo.com>
2022-10-30 16:43:01 -0500

    updated DOCKERFILE

--- home_management_system/chores/models.py
+++ home_management_system/chores/models.py
@@ -8,8 +8,8 @@ from accounts.models import FamilyMember
 class ChoreTemplate(models.Model):
     assigned_to = models.ForeignKey(FamilyMember, on_delete=models.CASCADE)
     day_assigned = models.CharField(max_length=20, default="day")
-    chore_name = models.CharField(max_length=20, default="chore")
-    note = models.TextField(max_length=80, blank=True, null=True)
+    chore_name = models.CharField(max_length=100, default="chore")
+    note = models.TextField(max_length=1000, blank=True, null=True)
     completed = models.BooleanField(default=False, null=True, blank=True)
     parents_completed = models.BooleanField(default=False, null=True, blank=True)
     repeated_chore = models.BooleanField(default=False, null=True, blank=True)
